f21b5fc5b9ad678bcf1fc7af8244e03147ed8a68
Chase Geigle <sky@skystrife.com>
2016-09-21 23:42:52 -0500

    Fix libc++experimental library name, improve messaging.

--- kludges/ExperimentalFilesystem.cmake
+++ kludges/ExperimentalFilesystem.cmake
@@ -1,35 +1,45 @@
 # experimental::filesystem is packaged as a separate static library in
 # GCC >= 5.3 as -lstdc++fs
 if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+  message("-- Locating libstdc++ filesystem library")
   find_library(STDCXX_FILESYSTEM
     NAMES stdc++fs
     HINTS ${CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES})
 
   if (STDCXX_FILESYSTEM)
-    message("-- Found libstdc++ filesystem: ${STDCXX_FILESYSTEM}")
-    set(CXX_EXPERIMENTAL_FILESYSTEM_LIBRARY ${STDCXX_FILESYSTEM})
+    message("-- Found libstdc++ filesystem library: ${STDCXX_FILESYSTEM}")
+    set(STD_EXPERIMENTAL_FILESYSTEM_LIBRARIES ${STDCXX_FILESYSTEM})
+  else()
+    message("-- Locating libstdc++ filesystem library - not found")
   endif()
 endif()
 
+# experimental::filesystem is packaged in the separate static library in
+# libc++ >= 3.9 as -lc++experimental
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND LIBCXX_LIBRARY)
+  message("-- Locating libc++experimental library")
   find_library(LIBCXX_EXPERIMENTAL
-    NAMES libc++experimental
+    NAMES c++experimental
     HINTS ${CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES} ${LIBCXX_LIB_PATH})
 
   if (LIBCXX_EXPERIMENTAL)
-    message("-- Found libc++experimental: ${LIBCXX_EXPERIMENTAL}")
+    message("-- Found libc++experimental library: ${LIBCXX_EXPERIMENTAL}")
     set(STD_EXPERIMENTAL_FILESYSTEM_LIBRARIES ${LIBCXX_EXPERIMENTAL})
+  else()
+    message("-- Locating libc++experimental library - not found")
   endif()
 endif()
 
-if (CXX_EXPERIMENTAL_FILESYSTEM_LIBRARY)
+if (STD_EXPERIMENTAL_FILESYSTEM_LIBRARIES)
+  message("-- Determining experimental filesystem library capabilities")
   set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES} ${STD_EXPERIMENTAL_FILESYSTEM_LIBRARIES}")
   check_cxx_source_compiles("
   #include <experimental/filesystem>
 
   int main()
   {
-      std::experimental::filesystem::path p1 = \"/usr\";
+      std::experimental::filesystem::path p1 = \"/home/meta/tmp/123456\";
+      std::experimental::filesystem::remove_all(p1);
       return 0;
   }" META_HAS_EXPERIMENTAL_FILESYSTEM)
 
@@ -64,4 +74,8 @@ if (CXX_EXPERIMENTAL_FILESYSTEM_LIBRARY)
     file(REMOVE_RECURSE ${META_REMOVE_ALL_TEST_DIR})
     unset(META_REMOVE_ALL_TEST_DIR)
   endif()
+endif()
+
+if (NOT META_HAS_EXPERIMENTAL_FILESYSTEM)
+  message("-- No suitable experimental filesystem library found")
 endif()
