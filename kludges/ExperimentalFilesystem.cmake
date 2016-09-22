83cd7573ba03418138fc207f65d35ae524738c37
Chase Geigle <geigle1@illinois.edu>
2016-09-21 21:53:08 -0500

    Add support for libc++experimental in ExperimentalFilesystem.cmake

--- kludges/ExperimentalFilesystem.cmake
+++ kludges/ExperimentalFilesystem.cmake
@@ -7,49 +7,61 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
 
   if (STDCXX_FILESYSTEM)
     message("-- Found libstdc++ filesystem: ${STDCXX_FILESYSTEM}")
-    set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES} ${STDCXX_FILESYSTEM}")
+    set(CXX_EXPERIMENTAL_FILESYSTEM_LIBRARY ${STDCXX_FILESYSTEM})
   endif()
 endif()
 
-check_cxx_source_compiles("
-#include <experimental/filesystem>
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND LIBCXX_LIBRARY)
+  find_library(LIBCXX_EXPERIMENTAL
+    NAMES libc++experimental
+    HINTS ${CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES} ${LIBCXX_LIB_PATH})
 
-int main()
-{
-    std::experimental::filesystem::path p1 = \"/usr\";
-    return 0;
-}" META_HAS_EXPERIMENTAL_FILESYSTEM)
-
-if (META_HAS_EXPERIMENTAL_FILESYSTEM)
-  target_compile_definitions(compiler-kludges INTERFACE
-    -DMETA_HAS_EXPERIMENTAL_FILESYSTEM)
-
-  if (STDCXX_FILESYSTEM)
-    target_link_libraries(compiler-kludges INTERFACE ${STDCXX_FILESYSTEM})
-    set(STD_EXPERIMENTAL_FILESYSTEM_LIBRARIES ${STDCXX_FILESYSTEM})
+  if (LIBCXX_EXPERIMENTAL)
+    message("-- Found libc++experimental: ${LIBCXX_EXPERIMENTAL}")
+    set(STD_EXPERIMENTAL_FILESYSTEM_LIBRARIES ${LIBCXX_EXPERIMENTAL})
   endif()
 endif()
 
-# experimental::filesystem::remove_all doesn't recurse properly as of GCC
-# 5.3.
-set(META_REMOVE_ALL_TEST_DIR ${CMAKE_CURRENT_BINARY_DIR}/meta-filesystem-test)
-file(MAKE_DIRECTORY ${META_REMOVE_ALL_TEST_DIR})
-file(MAKE_DIRECTORY ${META_REMOVE_ALL_TEST_DIR}/subdir)
-file(WRITE ${META_REMOVE_ALL_TEST_DIR}/subdir/file "Just some testing text")
+if (CXX_EXPERIMENTAL_FILESYSTEM_LIBRARY)
+  set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES} ${STD_EXPERIMENTAL_FILESYSTEM_LIBRARIES}")
+  check_cxx_source_compiles("
+  #include <experimental/filesystem>
 
-check_cxx_source_runs("
-#include <experimental/filesystem>
+  int main()
+  {
+      std::experimental::filesystem::path p1 = \"/usr\";
+      return 0;
+  }" META_HAS_EXPERIMENTAL_FILESYSTEM)
 
-int main()
-{
-    std::experimental::filesystem::remove_all(\"${META_REMOVE_ALL_TEST_DIR}\");
-    return 0;
-}" META_HAS_EXPERIMENTAL_FILESYSTEM_REMOVE_ALL)
+  if (META_HAS_EXPERIMENTAL_FILESYSTEM)
+    target_compile_definitions(compiler-kludges INTERFACE
+      -DMETA_HAS_EXPERIMENTAL_FILESYSTEM)
 
-if (META_HAS_EXPERIMENTAL_FILESYSTEM_REMOVE_ALL)
-  target_compile_definitions(compiler-kludges INTERFACE
-    -DMETA_HAS_EXPERIMENTAL_FILESYSTEM_REMOVE_ALL)
+    target_link_libraries(compiler-kludges INTERFACE
+      ${STD_EXPERIMENTAL_FILESYSTEM_LIBRARIES})
+
+    # experimental::filesystem::remove_all doesn't recurse properly as of GCC
+    # 5.3.
+    set(META_REMOVE_ALL_TEST_DIR ${CMAKE_CURRENT_BINARY_DIR}/meta-filesystem-test)
+    file(MAKE_DIRECTORY ${META_REMOVE_ALL_TEST_DIR})
+    file(MAKE_DIRECTORY ${META_REMOVE_ALL_TEST_DIR}/subdir)
+    file(WRITE ${META_REMOVE_ALL_TEST_DIR}/subdir/file "Just some testing text")
+
+    check_cxx_source_runs("
+    #include <experimental/filesystem>
+
+    int main()
+    {
+        std::experimental::filesystem::remove_all(\"${META_REMOVE_ALL_TEST_DIR}\");
+        return 0;
+    }" META_HAS_EXPERIMENTAL_FILESYSTEM_REMOVE_ALL)
+
+    if (META_HAS_EXPERIMENTAL_FILESYSTEM_REMOVE_ALL)
+      target_compile_definitions(compiler-kludges INTERFACE
+        -DMETA_HAS_EXPERIMENTAL_FILESYSTEM_REMOVE_ALL)
+    endif()
+
+    file(REMOVE_RECURSE ${META_REMOVE_ALL_TEST_DIR})
+    unset(META_REMOVE_ALL_TEST_DIR)
+  endif()
 endif()
-
-file(REMOVE_RECURSE ${META_REMOVE_ALL_TEST_DIR})
-unset(META_REMOVE_ALL_TEST_DIR)
